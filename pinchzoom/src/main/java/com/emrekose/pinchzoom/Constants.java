9b24e1451abda388f77da1dab87cb5a8c488d19c
Emre Köse <e.kose26@gmail.com>
2016-07-19 19:27:25 +0300

    Initial commit

---
+++ pinchzoom/src/main/java/com/emrekose/pinchzoom/Constants.java
@@ -0,0 +1,15 @@
+package com.emrekose.pinchzoom;
+
+/**
+ * Created by emrekose on 19.07.2016.
+ */
+public class Constants {
+    public static final int NONE = 0;
+    public static final int DRAG = 1;
+    public static final int ZOOM = 2;
+
+    private static final float MIN_ZOOM = 1f;
+    private static final float MAX_ZOOM = 5f;
+
+    public static final String TAG = "TOUCH";
+}
---
+++ pinchzoom/src/main/java/com/emrekose/pinchzoom/Touch.java
@@ -0,0 +1,17 @@
+package com.emrekose.pinchzoom;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by emrekose on 19.07.2016.
+ */
+public class Touch implements View.OnTouchListener {
+
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+}
